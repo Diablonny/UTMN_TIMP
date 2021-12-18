6b4f20f380365790f2abecdac8be7cae588d117a
Diablonny <41743601+Diablonny@users.noreply.github.com>
2021-12-19 00:58:06 +0500

    Create bad.cs

---
+++ Доп.Лабораторная №9/Структурные/Мост/bad.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Factory
+{
+
+    class Shape { }
+    
+    class Rectangle : Shape { }
+    class Circle : Shape { }
+
+    class RedRectangle : Rectangle { }
+    class GreenRectangle : Rectangle { }
+
+    class RedCircle : Circle { }
+    class GreenCircle : Circle { }
+
+    class Square : Shape { }
+
+    class RedSquare : Square { }
+    class GreenSquare : Square { }
+
+    class YellowSquare : Square { }
+    class YellowCircle : Circle { }
+    class YellowRectangle : Rectangle { }
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+           
+        }
+    }
+}

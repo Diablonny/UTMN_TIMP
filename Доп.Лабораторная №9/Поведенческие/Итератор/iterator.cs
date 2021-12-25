4b16be07ef7430f1b324e71680065b4884ee3daf
Diablonny <41743601+Diablonny@users.noreply.github.com>
2021-12-26 00:23:15 +0500

    Create iterator.cs

---
+++ Доп.Лабораторная №9/Поведенческие/Итератор/iterator.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp8
+{
+    class SimpleSet<T> : IEnumerator<T>
+    {
+        int index; //какой элемент нужно возвращать
+        readonly T[] storage;
+        public SimpleSet(params T[] args)
+        {
+            storage = args;
+            Reset();
+        }
+
+        public T Current => storage[index]; //возвращаем элемент хранилища
+
+        object IEnumerator.Current => Current;
+        public bool MoveNext() => --index >= 0; //метод узнает, дошли ли мы до конца хранилища
+        public void Reset() => index = storage.Length; //обнуление значения индекса, чтобы выводить элементы с начала
+                                              
+        //изменяя значения переменной index мы можем менять способ, как будет выводится наш массив
+        //допустим --index >= 2 и storage.Lenght - 1 выведет 5 4 3
+        public void Dispose() { } 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+
+            SimpleSet<int> set = new(array);
+
+            Console.WriteLine();
+
+            set.Reset();
+            while (set.MoveNext()) Console.Write($"{set.Current} "); 
+            //выведет 6 5 4 3 2 1
+
+            Console.WriteLine();
+        }
+    }
+}

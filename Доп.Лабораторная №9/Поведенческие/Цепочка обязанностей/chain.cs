58ebd66ad1e9fc3c61aa4628b6573b3c57dfdc93
Diablonny <41743601+Diablonny@users.noreply.github.com>
2021-12-25 21:50:10 +0500

    Create chain.cs

---
+++ Доп.Лабораторная №9/Поведенческие/Цепочка обязанностей/chain.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp8
+{
+    enum Responsibility
+    {
+        AutoAnswer = 1,
+        HelpCenter = 2,
+        Master = 3
+    }
+    abstract class Participant
+    {
+        public abstract void Conversation(string message, Responsibility level);
+    }
+    class AutoAnswer : Participant
+    {
+        readonly Responsibility priority;
+        public AutoAnswer(Responsibility priority)
+        {
+            this.priority = priority;
+        }
+        public Participant Next { get; set; }
+
+        public override void Conversation(string message, Responsibility level)
+        {
+            Console.WriteLine("Вы обратились на авто-ответчик");
+
+            if (level <= priority)
+            {
+                Console.WriteLine(message);
+            }
+            else if (Next != null)
+            {
+                Next.Conversation(message, level);
+            }
+        }
+    }
+    class HelpCenter : Participant
+    {
+        readonly Responsibility priority;
+        public HelpCenter(Responsibility priority)
+        {
+            this.priority = priority;
+        }
+        public Participant Next { get; set; }
+
+        public override void Conversation(string message, Responsibility level)
+        {
+            Console.WriteLine("Вы обратились в служебный центр");
+            if (level <= priority)
+            {
+                Console.WriteLine(message);
+            }
+            else if (Next != null)
+            {
+                Next.Conversation(message, level);
+            }
+        }
+    }
+    class Master : Participant
+    {
+        readonly Responsibility priority;
+        public Master(Responsibility priority)
+        {
+            this.priority = priority;
+        }
+        public Participant Next { get; set; }
+
+        public override void Conversation(string message, Responsibility level)
+        {
+            Console.WriteLine("Вы обратились к мастеру");
+
+            if (level <= priority)
+            {
+                Console.WriteLine(message);
+            }
+            else if (Next != null)
+            {
+                Next.Conversation(message, level);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoAnswer robot = new AutoAnswer(Responsibility.AutoAnswer);
+            Master injener = new Master(Responsibility.Master);
+            HelpCenter marija = new HelpCenter(Responsibility.HelpCenter);
+
+            robot.Next = marija;
+            injener.Next = robot;
+
+            robot.Conversation("Алло", Responsibility.AutoAnswer);
+            Console.WriteLine("\n");
+            robot.Conversation("Переключите на мастера", Responsibility.HelpCenter);
+            Console.WriteLine("\n");
+            robot.Conversation("Переключите на робота", Responsibility.Master);
+        }
+    }
+}
+
